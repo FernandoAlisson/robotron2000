1ab359664db777c21134b7e0dfab153e948624ab
FernandoAlisson <97045274+FernandoAlisson@users.noreply.github.com>
2023-02-16 14:44:11 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+/* 
+  FUNCOES
+
+  FUNCAO PADRAO
+  function funcao(){
+    console.log("sou uma funcao padrao")
+  }
+
+  FUNCAO ANONIMA - executada uma unica vez
+  function () {
+    console.log("sou uma funcao anonima")
+  }
+
+  FUNCAO ANONIMA ARROW FUNCTION - possivel escrever em uma unica linha, sintaxe mais simples
+  () => {console.log("sou uma arrow function anonima")}
+*/
+
+//const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+const pecas = {
+  "bracos": {
+      "forca": 29,
+      "poder": 35,
+      "energia": -21,
+      "velocidade": -5
+  },
+
+  "blindagem": {
+      "forca": 41,
+      "poder": 20,
+      "energia": 0,
+      "velocidade": -20
+  },
+  "nucleos":{
+      "forca": 0,
+      "poder": 7,
+      "energia": 48,
+      "velocidade": -24
+  },
+  "pernas":{
+      "forca": 27,
+      "poder": 21,
+      "energia": -32,
+      "velocidade": 42
+  },
+  "foguetes":{
+      "forca": 0,
+      "poder": 28,
+      "energia": 0,
+      "velocidade": -2
+  }
+}
+
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    atualizaEstatisticas(evento.target.dataset.peca)
+  })
+})
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]")
+  
+  if(operacao === "-") {
+    if(peca.value > 0) {
+      peca.value = parseInt(peca.value) - 1
+    }
+  } else {
+    peca.value = parseInt(peca.value) + 1
+  }
+}
+
+function atualizaEstatisticas(peca) {
+
+  estatisticas.forEach( (elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
+  })
+}
